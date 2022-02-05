dba4674ca902445925ba8d40ec4f1978ec19aceb
harryodubhghaill <harryodubhghaill@hotmail.com>
2022-02-05 12:20:09 +0000

    Fix increment to decrement in drawGameToken so tokens fill from bottom

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,15 +8,15 @@ function drawGameBoard(){
     ctx.fillRect(100, 50, 600, 500);
 
     // draw grid of circles on game board
-    for (var i = 0; i < 6; i++) {
-        for (var j = 0; j < 7; j++) {
+    for (let i = 0; i < 6; i++) {
+        for (let j = 0; j < 7; j++) {
           ctx.beginPath();
-          var x = 155 + j * 80; // x coordinate
-          var y = 100 + i * 80; // y coordinate
-          var radius = 30; // Arc radius
-          var startAngle = 0; // Starting point on circle
-          var endAngle = Math.PI * 2; // End point on circle
-          var counterclockwise = true; // clockwise or counterclockwise
+          let x = 155 + j * 80; // x coordinate
+          let y = 100 + i * 80; // y coordinate
+          let radius = 30; // Arc radius
+          let startAngle = 0; // Starting point on circle
+          let endAngle = Math.PI * 2; // End point on circle
+          let counterclockwise = true; // clockwise or counterclockwise
 
           ctx.arc(x, y, radius, startAngle, endAngle, counterclockwise);
 
@@ -29,37 +29,42 @@ function drawGameBoard(){
 drawGameBoard()
 
 let coords = [
-    {col: 0, row: 0},
-    {col: 1, row: 0},
-    {col: 2, row: 0},
-    {col: 3, row: 0},
-    {col: 4, row: 0},
-    {col: 5, row: 0},
-    {col: 6, row: 0},
+    {col: 0, row: 5},
+    {col: 1, row: 5},
+    {col: 2, row: 5},
+    {col: 3, row: 5},
+    {col: 4, row: 5},
+    {col: 5, row: 5},
+    {col: 6, row: 5},
 ]
 
 function drawGameToken(player, row, col){
-    ctx.beginPath();
-    var x = 155 + col*80; // x coordinate
-    var y = 100 + row*80; // y coordinate
-    var radius = 30; // Arc radius
-    var startAngle = 0; // Starting point on circle
-    var endAngle = Math.PI * 2; // End point on circle
-    var counterclockwise = true; // clockwise or counterclockwise
-
-    ctx.arc(x, y, radius, startAngle, endAngle, counterclockwise);
-
-    // change color depending on player
-    if (player === 1) {
-        ctx.fillStyle = 'rgb(255,0,0)';
+    if (row < 0){
+        console.log('woops');
     } else {
-        ctx.fillStyle = 'rgb(255,204,0)';
+        ctx.beginPath();
+        let x = 155 + col*80; // x coordinate
+        let y = 100 + row*80; // y coordinate
+        let radius = 30; // Arc radius
+        let startAngle = 0; // Starting point on circle
+        let endAngle = Math.PI * 2; // End point on circle
+        let counterclockwise = true; // clockwise or counterclockwise
+
+        ctx.arc(x, y, radius, startAngle, endAngle, counterclockwise);
+
+        // change color depending on player
+        if (player === 1) {
+            ctx.fillStyle = 'rgb(255,0,0)';
+        } else {
+            ctx.fillStyle = 'rgb(255,204,0)';
+        }
+        ctx.fill();
+        // increments row value to place next token on top
+        coords[col].row--
     }
-    ctx.fill();
-    coords[col].row++
 }
 
-
+// funtion to take param for player turn, then draws token
 function playerTurn(player, selection){
     drawGameToken(player,coords[selection].row,coords[selection].col)
 }
@@ -70,7 +75,9 @@ playerTurn(1, 3)
 playerTurn(2, 0)
 playerTurn(1, 5)
 playerTurn(2, 1)
-
+playerTurn(2, 0)
+playerTurn(2, 0)
+playerTurn(2, 0)
 console.log(coords)
 
 
